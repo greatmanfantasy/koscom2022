6e5b4f6ee2b368c822bfc9f26fc6aad35e2bbe2c
Ubuntu <ubuntu@ip-172-31-16-186.ap-northeast-2.compute.internal>
2022-09-15 05:54:07 +0000

    Add contents on pages

--- frontend/src/pages/admin/mypage/termsOfService.vue
+++ frontend/src/pages/admin/mypage/termsOfService.vue
@@ -7,11 +7,18 @@
                     <div class="flex">
                         <va-card>
                             <va-card-content>
+                                <div class="grid row">
                                 <div class="flex md12" style="text-align: center; font-size:24px;">
                                     약관 동의
-                                    </div>
-                                                      
-
+                                </div>
+                                <div class="flex md8 offset--md2">
+                                    <va-checkbox v-model="check_bank" label="은행정보 수집 이용동의 (필수)" class="mb-2" />
+                                    <va-checkbox v-model="check_card" label="카드사정보 수집 이용동의 (필수)" class="mb-2" />
+                                    <va-checkbox v-model="check_insurance" label="보험사정보 수집 이용동의 (필수)" class="mb-2" />
+                                    <va-checkbox v-model="check_stock" label="증권사정보 수집 이용동의 (필수)" class="mb-2" />
+                                    <va-checkbox v-model="check_tax" label="국세청정보 수집 이용동의 (필수)" class="mb-2" />
+                                </div>
+                            </div>
                             </va-card-content>
                         </va-card>
 
@@ -36,6 +43,7 @@
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { defineProps } from 'vue'
 const router = useRouter();
 
 const users = [
@@ -60,8 +68,16 @@ function getStatusColor(status: string) {
 
 
 function onsubmit() {
-    router.push({ name: 'anticipate' })
+    router.push({ name: 'loading' })
 }
+
+const props = defineProps({
+    check_bank: Boolean,
+    check_card: Boolean,
+    check_insurance: Boolean,
+    check_stock: Boolean,
+    check_tax: Boolean,
+})
 
 </script>
 
