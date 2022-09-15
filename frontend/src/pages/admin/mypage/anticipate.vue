762548412aba8e94e92dadcf5abd171a080ab821
Ubuntu <ubuntu@ip-172-31-16-186.ap-northeast-2.compute.internal>
2022-09-15 05:30:56 +0000

    Add contents of ant page

--- frontend/src/pages/admin/mypage/anticipate.vue
+++ frontend/src/pages/admin/mypage/anticipate.vue
@@ -10,8 +10,16 @@
                                 <div class="flex md12" style="text-align: center; font-size:24px;">
                                     인건님의 예상 환급금액
                                 </div>
-                                <div> 1,232,300 원 추가납부 </div>
-                                <div>
+
+                                <div class="flex md12" style="text-align: center; font-size:30px;">
+                                    <b>1,232,300 </b> 원 추가납부
+                                </div>
+
+                                <div class="flex md12" style="text-align: center; font-size:20px;">
+                                    예상세액 : 25,630,000 예상납부세액 : 24,080,000
+                                </div>
+
+                                <!-- <div>
                                     <va-card>
                                         <va-card-title></va-card-title>
                                         <va-card-content>
@@ -60,7 +68,7 @@
                                         </va-card-content>
                                     </va-card>
 
-                                </div>
+                                </div> -->
 
                             </va-card-content>
                         </va-card>
@@ -70,6 +78,59 @@
                 </div>
             </div>
 
+
+            <div class="flex md6">
+                <div class="row column">
+                    <div class="flex">
+                        <va-card>
+                            <va-card-content>
+                                <div>상세내역</div>
+                                <div>근로소득세 23,000,000 원</div>
+                                <div>이자소득세 470,000 원</div>
+                                <div>
+                                    <div>▼ (예상)배당소득세 23,500원</div>
+                                    <div>- 삼성전자 57,000원 20주</div>
+                                    <div>12월 배당예상액 : 12,000원</div>
+                                    <div>- KB금융 42,000원 15주</div>
+                                    <div>10월 배당예상액 : 5,700원</div>
+                                    <div>12월 배당예상액 : 5,800원</div>
+                                </div>
+
+                                <div>
+                                    <br>
+                                </div>
+                                
+                                <div></div>
+
+                                <div>
+                                    <div>인적공제 160,000 원</div>
+                                    <div>기타 및 특별공제 1,009,000 원</div>
+                                </div>                                
+                            </va-card-content>
+                        </va-card>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="flex md6">
+                <div class="row column">
+                    <div class="flex">
+                        <va-card>
+                            <va-card-content>
+                                <div>나의 절세 현황</div>
+                                <div>청약저축 : 230,000 원 공제</div>
+                                <div>연금저축 : 1,080,000 원 공제</div>
+                                <div>체크카드 : 2,756,000 원 공제</div>
+                                <div>정치기부금 : 100,000 원 공제</div>
+                                <div>대중교통 : 97,000 원 공제</div>
+                            </va-card-content>
+                        </va-card>
+                    </div>
+
+                </div>
+            </div>
 
             <div class="flex xs12">
                 <div class="row">
